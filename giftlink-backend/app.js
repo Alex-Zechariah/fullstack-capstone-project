f377f521606035652fbf0169c2fc0b481d1ac275
Alex-Zechariah <alexzechariah04@gmail.com>
2025-09-20 15:03:03 -0400

    feat: implement API for search service

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,13 +1,15 @@
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-const connectToDatabase = require('./models/db'); 
+const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 const app = express();
 app.use("*",cors());
 const port = 3060;
 
+// Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
@@ -15,14 +17,19 @@ connectToDatabase().then(() => {
 
 app.use(express.json());
 
+// Route files
 const giftRoutes = require('./routes/giftRoutes');
+// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
-
 app.use(pinoHttp({ logger }));
 
+// Use Routes
 app.use('/api/gifts', giftRoutes);
+// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
